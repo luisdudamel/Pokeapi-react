f4ac1fbb5d58e1759c9d0041b10fc9490eba11e7
Luis Dudamel <luis.dudamel.ext@idrica.com>
2023-10-21 17:21:14 +0200

    Add PokemonList test for showing elements

--- src/mocks/pokemon.ts
+++ src/mocks/pokemon.ts
@@ -1,9 +1,17 @@
 import { Pokemon } from "../types/pokemon";
 
-export const mockPokemon: Pokemon = {
+export const mockPikachu: Pokemon = {
     name: "Pikachu",
     type: "Electric",
     weight: 23,
     index: 2,
     image: "pikachu.jpg",
+};
+
+export const mockCharizard: Pokemon = {
+    name: "Charizard",
+    type: "Fire",
+    weight: 23,
+    index: 2,
+    image: "charizard.jpg",
 };
